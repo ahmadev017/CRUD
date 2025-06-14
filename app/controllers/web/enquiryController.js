330d13ae9d4de00a4035e3fbde3699e237305187
ahmadev017 <ahmadev01708@gmail.com>
2025-06-14 12:16:32 +0500

    d

--- app/controllers/web/enquiryController.js
+++ app/controllers/web/enquiryController.js
@@ -1,57 +1,68 @@
-const enquiryModel =require("../../models/enquiry.model")
+const enquiryModel = require("../../models/enquiry.model");
 
+let enquiryInsert = (req, res) => {
+  let { name, email, phone, message } = req.body;
+  let enquiry = new enquiryModel({ name, email, phone, message });
 
+  enquiry
+    .save()
+    .then(() => {
+      res.send({ status: 1, message: "Enquiry saved successfully" });
+    })
+    .catch((err) => {
+      res.send({ status: 0, message: "Error while saving", error: err });
+    });
+};
 
+let enquiryList = async (req, res) => {
+  try {
+    let enquiry = await enquiryModel.find();
+    res.send({ status: 1, enquiryList: enquiry });
+  } catch (err) {
+    console.error("Error in enquiryList:", err);
+    res.status(500).send({ status: 0, message: "Internal Server Error", error: err.message });
+  }
+};
 
-let enquiryInsert=(req,res)=>
-{
-     let {name, email, phone, message}=req.body;
-     let enquiry= new enquiryModel({
-      name,
-      email,
-      phone,
-      message
-     })
-     enquiry.save().then(()=>{
-      res.send({status:1, message:"Enquiry save successfully"});
-     }).catch((err)=>{
-      res.send({status:0, message:"Error while saving successfully",error:err});
-     })
-}
+let enquiryDelete = async (req, res) => {
+  try {
+    let enId = req.params.id;
+    let enquiry = await enquiryModel.deleteOne({ _id: enId });
+    res.send({ status: 1, message: "Enquiry deleted successfully", enquiry });
+  } catch (err) {
+    console.error("Error in enquiryDelete:", err);
+    res.status(500).send({ status: 0, message: "Internal Server Error", error: err.message });
+  }
+};
 
+let enquirySingleRow = async (req, res) => {
+  try {
+    let enId = req.params.id;
+    let enquiry = await enquiryModel.findOne({ _id: enId });
+    res.send({ status: 1, enquiry });
+  } catch (err) {
+    console.error("Error in enquirySingleRow:", err);
+    res.status(500).send({ status: 0, message: "Internal Server Error", error: err.message });
+  }
+};
 
+let enquiryUpdate = async (req, res) => {
+  try {
+    let enquiryId = req.params.id;
+    let { name, email, phone, message } = req.body;
+    let updateObj = { name, email, phone, message };
+    let updateRes = await enquiryModel.updateOne({ _id: enquiryId }, updateObj);
+    res.send({ status: 1, message: "Enquiry updated successfully", updateRes });
+  } catch (err) {
+    console.error("Error in enquiryUpdate:", err);
+    res.status(500).send({ status: 0, message: "Internal Server Error", error: err.message });
+  }
+};
 
-let enquiryList =async (req,res)=>{
-     let enquiry = await enquiryModel.find();
-     res.send({status:1, enquiryList:enquiry});
-}
-
-
-let enquiryDelete =async (req,res)=>{
-      let enId = req.params.id;
-      let enquiry=await enquiryModel.deleteOne({_id:enId});
-      res.send({status:1, message:"Enquiry deleted successfully",enquiry})
-}
-
-
-let enquirySingleRow =async (req,res)=>{
-     let enId=req.params.id;
-     let enquiry = await enquiryModel.findOne({_id:enId})
-     res.send({status:1, enquiry});
-}
-
-let enquiryUpdate = async (req,res)=>{
-     let enquiryId=req.params.id;
-     let {name, email, phone, message}=req.body;
-     let updateObj={
-          name,
-          email,
-          phone,
-          message
-     };
-     let updateRes=await enquiryModel.updateOne({_id:enquiryId},updateObj)
-     res.send({status:1,message:"Enquiry updated successfully",updateRes})
-
-}
-
-module.exports={enquiryInsert,enquiryList,enquirySingleRow, enquiryDelete,enquiryUpdate};
+module.exports = {
+  enquiryInsert,
+  enquiryList,
+  enquirySingleRow,
+  enquiryDelete,
+  enquiryUpdate,
+};
